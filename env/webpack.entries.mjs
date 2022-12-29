d33c42fdca7185a020ff32fdd8d5febabc193c1f
fritylo <to.fritylo@gmail.com>
2022-12-29 23:00:00 +0300

    feat(env): change indent from sapces to tabs in env folder

--- env/webpack.entries.mjs
+++ env/webpack.entries.mjs
@@ -15,42 +15,42 @@ import { getOutputFilename } from './webpack.lib.mjs';
  * }}
  */
 export const entries = Object.fromEntries([].concat(
-    glob.sync('./src/pages/*/*.ts').map(filePath => {
-        const pageName = basename(filePath).replace(/\.ts$/, '');
-        
-        return [`page-${pageName}`, {
-            entry: filePath,
-            output: {
-                js: getOutputFilename(filePath, './js', '.js'),
-                css: getOutputFilename(filePath, './css', '.css'),
-                php: () => getOutputFilename(filePath, '', '.php'),
-            },
-        }];
-    }),
-    glob.sync('./src/raw-loader.ts').map(filePath => {
-        return ['raw', {
-            entry: filePath,
-            output: {
-                js: './raw/no-output',
-                php: (fileNameNoExt) => `${fileNameNoExt}.php`,
-            },
-        }];
-    }),
-    glob.sync('./src/assets-loader.ts').map(filePath => {
-        return ['asset', {
-            entry: filePath,
-            output: {
-                js: './assets/no-output',
-            },
-        }];
-    }),
-    glob.sync('./src/components/common/entry.ts').map(filePath => {
-        return ['common', {
-            entry: filePath,
-            output: {
-                js: getOutputFilename(filePath, './components/common', '.js'),
-                css: getOutputFilename(filePath, './components/common', '.css'),
-            },
-        }];
-    }),
+	glob.sync('./src/pages/*/*.ts').map(filePath => {
+		const pageName = basename(filePath).replace(/\.ts$/, '');
+
+		return [`page-${pageName}`, {
+			entry: filePath,
+			output: {
+				js: getOutputFilename(filePath, './js', '.js'),
+				css: getOutputFilename(filePath, './css', '.css'),
+				php: () => getOutputFilename(filePath, '', '.php'),
+			},
+		}];
+	}),
+	glob.sync('./src/raw-loader.ts').map(filePath => {
+		return ['raw', {
+			entry: filePath,
+			output: {
+				js: './raw/no-output',
+				php: (fileNameNoExt) => `${fileNameNoExt}.php`,
+			},
+		}];
+	}),
+	glob.sync('./src/assets-loader.ts').map(filePath => {
+		return ['asset', {
+			entry: filePath,
+			output: {
+				js: './assets/no-output',
+			},
+		}];
+	}),
+	glob.sync('./src/components/common/entry.ts').map(filePath => {
+		return ['common', {
+			entry: filePath,
+			output: {
+				js: getOutputFilename(filePath, './components/common', '.js'),
+				css: getOutputFilename(filePath, './components/common', '.css'),
+			},
+		}];
+	}),
 ));
